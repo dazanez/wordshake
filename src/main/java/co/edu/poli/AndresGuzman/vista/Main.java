97d6b78cf3ecf7239063be7c70f161c2c2b5bb62
dazanez <dasl.672004@gmail.com>
2025-04-15 17:11:31 -0500

    Ajustes para generar ejecutable JAR

---
+++ src/main/java/co/edu/poli/AndresGuzman/vista/Main.java
@@ -0,0 +1,12 @@
+package co.edu.poli.AndresGuzman.vista;
+
+/* <<<<<<< IMPORTANTE >>>>>>>>
+
+NO BORRAR ARCHIVO - Se utiliza para la compilación del ejecutable JAR
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
